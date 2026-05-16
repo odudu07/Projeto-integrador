8093f75cd61c1d3e835ee3d72ecbfc009d279a8a
odudu07 <eduardo.arocha2024@gmail.com>
2026-05-15 21:35:19 -0300

    Projeto

--- js/banco.js
+++ js/banco.js
@@ -1,110 +1,21 @@
-// ===============================
-// BANCO DE DADOS LOCAL
-// ===============================
+let funcionarios = [
 
-// Cria funcionários padrões
-// Só cria se ainda não existir no localStorage
+    {
+        usuario: "Eduardo",
+        senha: "123",
+        cargo: "Chefe"
+    },
 
-if(localStorage.getItem("funcionarios") == null){
+    {
+        usuario: "joao",
+        senha: "456",
+        cargo: "RH"
+    },
 
-    let funcionarios = [
-
-        {
-            usuario: "Eduardo",
-            senha: "123"
-        },
-
-        {
-            usuario: "joao",
-            senha: "456"
-        },
-
-        {
-            usuario: "maria",
-            senha: "789"
-        }
-
-    ]
-
-    localStorage.setItem(
-        "funcionarios",
-        JSON.stringify(funcionarios)
-    )
-}
-
-
-
-// ===============================
-// SISTEMA DE LOGIN
-// ===============================
-
-document.getElementById("formLogin").addEventListener("submit", function(event){
-
-    // Impede recarregar página
-    event.preventDefault()
-
-
-    // Pega dados digitados
-    let usuarioDigitado =
-    document.getElementById("usuario").value
-
-    let senhaDigitada =
-    document.getElementById("senha").value
-
-
-    // Busca funcionários salvos
-    let funcionarios =
-    JSON.parse(localStorage.getItem("funcionarios"))
-
-
-    // Variável para verificar login
-    let loginCorreto = false
-
-
-    // Percorre todos funcionários
-    for(let i = 0; i < funcionarios.length; i++){
-
-        // Verifica usuário e senha
-        if(
-            usuarioDigitado === funcionarios[i].usuario &&
-            senhaDigitada === funcionarios[i].senha
-        ){
-
-            loginCorreto = true
-
-            // Salva usuário logado
-            localStorage.setItem(
-                "usuarioLogado",
-                funcionarios[i].usuario
-            )
-
-            break
-        }
+    {
+        usuario: "maria",
+        senha: "789",
+        cargo: "Funcionario"
     }
 
-
-
-    // Login correto
-    if(loginCorreto){
-
-        document.getElementById("mensagem").innerText =
-        "Login realizado com sucesso"
-
-
-        setTimeout(function(){
-
-            window.location.href =
-            "tela-inicial.html"
-
-        }, 2000)
-
-    }
-
-    // Login incorreto
-    else{
-
-        document.getElementById("mensagem").innerText =
-        "Usuário ou senha incorretos"
-    }
-
-})
+]
